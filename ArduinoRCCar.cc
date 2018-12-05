8dd837ae9dd57bbad96a4704d5b9e6d50e19e1bf
RoemIko <45184251+RoemIko@users.noreply.github.com>
2018-12-05 21:45:00 +0100

    Update ArduinoRCCar.cc

--- ArduinoRCCar.cc
+++ ArduinoRCCar.cc
@@ -7,7 +7,7 @@ int Left = 5;
 int Right = 4;
 int Forwards = 3;
 int Backwards = 2;
-int potPin = A0;
+int const potPin = A0;
 int speed = 0;
 #define SONAR_NUM 3
 #define TRIGGER_PIN1 11 //Front 
@@ -25,11 +25,11 @@ NewPing sonar[SONAR_NUM] = {
   NewPing(TRIGGER_PIN3, ECHO_PIN3, MAX_DISTANCE)
 };
 
-long sensors[3];
+long sensors[3]; //Makesalist of the sensor values and stores them in there so it can recall it back
 //Start of the program.
 void setup() {
   Serial.begin(9600);
-  pinMode(Left, OUTPUT);
+  pinMode(Left, OUTPUT); //setting the output of the pins
   pinMode(Right, OUTPUT);
   pinMode(Forwards, OUTPUT);
   pinMode(Backwards, OUTPUT);
@@ -37,17 +37,18 @@ void setup() {
 
 //During the program.
 void loop() {
+  
   for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through each sensor and display results.
     delay(50); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
     sensors[i] = sonar[i].ping_cm(); 
   }
 
   //Value of the potentiometer
-  speed = analogRead(potPin);
-  speed = map(speed, 0, 1023, 0, 179);
+  speed = analogRead(potPin); //reads the values of the pot meter. Sends off a signal of it
+  speed = map(speed, 0, 1023, 0, 179); 
 
   //Values for driving
-  if (sensors[0] > 20 ) {
+  if (sensors[0] > 50 ) { //if the distance of the front sensor is higher than 50cm, than set Fwd true. Otherwise its false.
     Fwd = true;
     //Serial.print(Fwd);
   } else {
@@ -55,25 +56,25 @@ void loop() {
     //Serial.print(Fwd);
   }
   delay(50);
- if ((Fwd == true) && (sensors[1] > sensors[2])) {
+ if ((Fwd == true) && (sensors[1] > sensors[2])) { //if Fwd = true then AND left is bigger than right, then call the function. 
     fwdLeft();
   } else if ((Fwd == true) && (sensors[2] > sensors[1])){
     fwdRight();
   }
-  delay(50);
   if (Fwd == false) {
     bwd();
   }
-  Serial.println(sensors[2]);
+  //Serial.println(sensors[2]);
 }
+//Functions to call back
 void fwdLeft() {
-  digitalWrite(Forwards, HIGH);
+  digitalWrite(Forwards, speed);
   digitalWrite(Backwards, LOW);
   digitalWrite(Left, HIGH);
   digitalWrite(Right, LOW);
 }
 void fwdRight() {
-  digitalWrite(Forwards, HIGH);
+  digitalWrite(Forwards, speed);
   digitalWrite(Backwards, LOW);
   digitalWrite(Left, LOW);
   digitalWrite(Right, HIGH);
@@ -86,8 +87,7 @@ void Stp() {
 }
 void bwd() {
   digitalWrite(Forwards, LOW);
-  digitalWrite(Backwards, HIGH);
+  digitalWrite(Backwards, speed);
   digitalWrite(Left, LOW);
   digitalWrite(Right, LOW);
 }
-
